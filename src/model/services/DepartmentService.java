a7ab766a351765e958dd2267f078e37e9b5dd3d7
fabiomendes123 <fabiomendes@live.com>
2020-06-10 11:18:14 -0300

    Initializing action as parameter

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -9,7 +9,7 @@ public class DepartmentService {
 	
 	public List<Department> findAll(){
 		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "books"));
+		list.add(new Department(1, "Books"));
 		list.add(new Department(2, "Computers"));
 		list.add(new Department(3, "Electronics"));
 		return list;
